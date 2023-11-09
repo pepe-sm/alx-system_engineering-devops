ada66be84327a1fb5de6c83b360b2cb654acd5de
Stephen Mburu <stephenpepe.sm@gmail.com>
2023-11-09 11:38:45 -0800

    added header and restricted redirection

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,20 +2,16 @@
 """top 10"""
 import requests
 
+headers = {"User-Agent": "MyCustomUserAgent/1.0"}
 
 def top_ten(subreddit):
     """function queries the Reddit API limit 10"""
 
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    req = requests.get(url)
-
-    if (req.status_code != 200):
-        print(None)
-        return
-
-    try:
-        childrens = req.json().get('data').get('children')
-        for children in childrens:
-            print(children.get('data').get('title'))
-    except Exception:
-        pass
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    response = requests.get(url, allow_redirects=False, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        for post in data["data"]["children"]:
+            print(post["data"]["title"])
+    else:
+        print("None")
