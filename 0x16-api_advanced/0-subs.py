0e5044fc28ba7d1ba76e781ec40734f2cb5701ca
Stephen Mburu <stephenpepe.sm@gmail.com>
2023-11-09 11:28:23 -0800

    query reddit api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,13 +2,15 @@
 """query reddit_api"""
 import requests
 
+headers = {"User-Agent": "MyCustomUserAgent/1.0"}
 
 def number_of_subscribers(subreddit):
     """function queries the Reddit API"""
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    req = requests.get(url)
-    try:
-        return req.json().get("data").get("subscribers")
-    except Exception:
-            return 0
+    response = requests.get(url, allow_redirects=False, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
+    else:
+        return 0
